2e7f3d74885dfe418a9f5e529a64b4bf1d3f1746
Mohammed Saeed <mohammed.elsaeed1@Gmail.com>
2020-12-04 16:05:20 +0200

    Updates

---
+++ precache-manifest.f3df2c1daf50ea354680d1173bb09682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957888b58fa24374181c814c7c7b72eb",
+    "url": "/ProShop/index.html"
+  },
+  {
+    "revision": "f05c33a92b98db60c8be",
+    "url": "/ProShop/static/css/main.0838601a.chunk.css"
+  },
+  {
+    "revision": "5a83c2d59b667d5ed702",
+    "url": "/ProShop/static/js/2.d8ca1f4c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ProShop/static/js/2.d8ca1f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05c33a92b98db60c8be",
+    "url": "/ProShop/static/js/main.c3ed4c44.chunk.js"
+  },
+  {
+    "revision": "1d7c12c9b0b7a799a5a4",
+    "url": "/ProShop/static/js/runtime-main.527ee25a.js"
+  }
+]);
